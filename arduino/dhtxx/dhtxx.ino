832a454dbcc07bfbdf7c1ce73d93788417c366b7
Kaleb Clark <kalebclark@kickstart-audio.attlocal.net>
2018-12-17 20:09:47 -0800

    Pre-publish - Arduino

--- arduino/dhtxx/dhtxx.ino
+++ arduino/dhtxx/dhtxx.ino
@@ -7,7 +7,7 @@ int dhtPin = 2;             // Set pin to pin 2 (digital)
 int dly = 2000;             // Define delay of 2000ms (2 seconds)
 
 //#define DHT_TYPE DHT11      // Uncomment if using the blue (dht11) sensor
-#define DHT_TYPE DHT22    // Uncomment if using the white (dht22) sensor
+#define DHT_TYPE DHT22      // Uncomment if using the white (dht22) sensor
 
 DHT dht(dhtPin, DHT_TYPE);  // Instantiate the DHT object from the library
 
@@ -16,8 +16,7 @@ void setup() {
   Serial.begin(19200);
   Serial.println("Makers Digest: Ready");
 
-  pinMode(dhtPin, INPUT_PULLUP);  // Tell arduino we are using pullup resistor
-  dht.begin();                    // Begin DHT;
+  dht.begin();              // Begin DHT;
 }
 
 void loop() {
@@ -30,6 +29,7 @@ void loop() {
   // return value in fahrenheit. 
   // Note 2: There are several other functions. Check out the library
   // at: https://github.com/adafruit/DHT-sensor-library
+  
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature(true);
 
